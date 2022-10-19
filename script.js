edfb1077136ba6c41caf1024bfac9c67cc719d13
Winfredo <winfrednukpe2002@gmail.com>
2022-10-19 14:13:56 +0000

    Changes

---
+++ script.js
@@ -0,0 +1,44 @@
+//    document.getElementById("count").innerHTML = 5;
+
+// let count = 0
+// count = count + 1
+
+ 
+//  console.log(count)  
+
+
+//  let bonusPoints = 50;
+// bonusPoints = bonusPoints + 50
+
+// console.log(bonusPoints)
+
+// bonusPoints = bonusPoints - 75
+// console.log(bonusPoints)
+
+// bonusPoints = bonusPoints + 45
+// console.log(bonusPoints)
+
+// function countdown(){
+//     console.log(5)
+//     console.log(5)
+//     console.log(5)
+//     console.log(5)
+//     console.log(5)
+// }
+
+// countdown()
+
+function laps(){
+
+
+
+let lapsCompleted = 0
+
+lapsCompleted = lapsCompleted + 1;
+
+console.log()
+}
+
+laps()
+laps()
+laps()
