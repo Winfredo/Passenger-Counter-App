401bdde031340b474cfbceb8b49c294eee30ec7c
Winfredo <winfrednukpe2002@gmail.com>
2022-10-20 17:07:46 +0000

    Changes

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
-
 let countEl = document.getElementById("count-element")
-
-console.log(countEl)
-
 
 
 let count = 0
 
-
-function increments(){
+function increments() {
     count = count + 1
     countEl.innerHTML = count
     console.log(count)
 }
+let countDel = document.getElementById("reset-btn")
 
+
+function decrements(){
+    let counts = 0
+    countDel.innerHTML = counts
+    console.log(counts)
+
+}
